3d39325e786b8181281b263df7e48b9e2d4a0959
Bruno Arruda <brunoarrudaacademy@gmail.com>
2025-06-20 08:20:39 -0300

    Added exercise 2.14

---
+++ Chapters/cp2/2.14/DisplayNumbers.java
@@ -0,0 +1,15 @@
+public class DisplayNumbers {
+    public static void main(String[] args) {
+        //println
+        System.out.println("12 34");
+
+        //print
+        System.out.print("1");
+        System.out.print("2");
+        System.out.print(" 3");
+        System.out.print("4");
+
+        //printf
+        System.out.printf("%n%d%d %d%d",1,2,3,4);
+    }
+}
